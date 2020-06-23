d134e48a8a906f6beb2313c21d5c7611acf9bd48
bambou <yannick.bonnefond@gmail.com>
2020-06-23 09:25:02 +0200

    chore: fix release

--- docs/js/menu-wc.js
+++ docs/js/menu-wc.js
@@ -47,69 +47,6 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                     </ul>
                 </li>
                     <li class="chapter">
-                        <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ? 'data-target="#classes-links"' :
-                            'data-target="#xs-classes-links"' }>
-                            <span class="icon ion-ios-paper"></span>
-                            <span>Classes</span>
-                            <span class="icon ion-ios-arrow-down"></span>
-                        </div>
-                        <ul class="links collapse " ${ isNormalMode ? 'id="classes-links"' : 'id="xs-classes-links"' }>
-                            <li class="link">
-                                <a href="classes/Debugger.html" data-type="entity-link">Debugger</a>
-                            </li>
-                            <li class="link">
-                                <a href="classes/Output.html" data-type="entity-link">Output</a>
-                            </li>
-                            <li class="link">
-                                <a href="classes/Route.html" data-type="entity-link">Route</a>
-                            </li>
-                            <li class="link">
-                                <a href="classes/Stubborn.html" data-type="entity-link">Stubborn</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="chapter">
-                        <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ? 'data-target="#interfaces-links"' :
-                            'data-target="#xs-interfaces-links"' }>
-                            <span class="icon ion-md-information-circle-outline"></span>
-                            <span>Interfaces</span>
-                            <span class="icon ion-ios-arrow-down"></span>
-                        </div>
-                        <ul class="links collapse " ${ isNormalMode ? ' id="interfaces-links"' : 'id="xs-interfaces-links"' }>
-                            <li class="link">
-                                <a href="interfaces/BodyDefinitionArray.html" data-type="entity-link">BodyDefinitionArray</a>
-                            </li>
-                            <li class="link">
-                                <a href="interfaces/DiffError.html" data-type="entity-link">DiffError</a>
-                            </li>
-                            <li class="link">
-                                <a href="interfaces/JsonArray.html" data-type="entity-link">JsonArray</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="chapter">
-                        <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ? 'data-target="#miscellaneous-links"'
-                            : 'data-target="#xs-miscellaneous-links"' }>
-                            <span class="icon ion-ios-cube"></span>
-                            <span>Miscellaneous</span>
-                            <span class="icon ion-ios-arrow-down"></span>
-                        </div>
-                        <ul class="links collapse " ${ isNormalMode ? 'id="miscellaneous-links"' : 'id="xs-miscellaneous-links"' }>
-                            <li class="link">
-                                <a href="miscellaneous/enumerations.html" data-type="entity-link">Enums</a>
-                            </li>
-                            <li class="link">
-                                <a href="miscellaneous/functions.html" data-type="entity-link">Functions</a>
-                            </li>
-                            <li class="link">
-                                <a href="miscellaneous/typealiases.html" data-type="entity-link">Type aliases</a>
-                            </li>
-                            <li class="link">
-                                <a href="miscellaneous/variables.html" data-type="entity-link">Variables</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="chapter">
                         <a data-type="chapter-link" href="coverage.html"><span class="icon ion-ios-stats"></span>Documentation coverage</a>
                     </li>
             </ul>
