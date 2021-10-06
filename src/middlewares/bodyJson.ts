18114532d53ad4203f58435739311211600b7fda
Tonton <yannick.bonnefond@gmail.com>
2021-10-06 12:08:26 +0200

    feat: support AWS JSON 1.0 and 1.1 content-types

--- src/middlewares/bodyJson.ts
+++ src/middlewares/bodyJson.ts
@@ -3,5 +3,12 @@ import { json } from 'body-parser';
  * @internal
  */
 export function bodyJson() {
-  return json();
+  return json({
+    type: [
+      'json',
+      'application/json',
+      'application/x-amz-json-1.0',
+      'application/x-amz-json-1.1',
+    ],
+  });
 }
