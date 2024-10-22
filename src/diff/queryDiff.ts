1ffaa08a2bc4b5ba97ab48d644c775b87d26b23b
Yannick B <yannickb@voodoo.io>
2024-10-22 18:10:15 +0200

    refactor: use object params for checkValue

--- src/diff/queryDiff.ts
+++ src/diff/queryDiff.ts
@@ -11,48 +11,54 @@ export function queryDiff(
   definition: QueryDefinition,
   value: RequestQuery,
 ): DiffError[] {
-  const validate: ValidateFn = (def: any, val: any, path: string) => {
-    if (!Array.isArray(def)) {
-      return checkParameters(def, val, path);
+  const validate: ValidateFn = (params: {
+    definition: any;
+    value: any;
+    path: string;
+  }) => {
+    if (!Array.isArray(params.definition)) {
+      return checkParameters(params);
     }
 
     // Definition is an array, but value is not
-    if (!Array.isArray(val)) {
+    if (!Array.isArray(params.value)) {
       return [
         formatDiffError({
+          ...params,
           type: DIFF_TYPES.INVALID_VALUE_TYPE,
-          definition: def,
-          value: val,
-          path,
         }),
       ];
     }
 
     return findErrors({
-      definition: def,
-      value: val,
+      definition: params.definition,
+      value: params.value,
+      prefix: params.path,
       validate: checkParameters,
-      prefix: path,
     });
   };
 
   return findErrors({ definition, value, validate, prefix: '' });
 }
 
-function checkParameters(
-  definition: QueryParameterDefinition,
-  value: string | string[],
-  path: string,
-) {
+function checkParameters({
+  definition,
+  value,
+  path,
+}: {
+  definition: QueryParameterDefinition;
+  value: string | string[];
+  path: string;
+}) {
   if (Array.isArray(value)) {
     return value.reduce((acc: DiffError[], val, i) => {
-      checkValue(definition, val, `${path}.${i}`).forEach(error =>
-        acc.push(error),
+      checkValue({ definition, value: val, path: `${path}.${i}` }).forEach(
+        error => acc.push(error),
       );
 
       return acc;
     }, []);
   }
 
-  return checkValue(definition, value, path);
+  return checkValue({ definition, value, path });
 }
