fbe57f0862fff5521b066687314ce7c200648cfb
Yannick B <yannickb@voodoo.io>
2024-10-23 11:28:04 +0200

    chore: remove duplicated test

--- test/specs/global.spec.ts
+++ test/specs/global.spec.ts
@@ -3,7 +3,7 @@ import { Debugger } from '../../src/debug/Debugger';
 import { toReplyWith } from '../matchers';
 import { test } from '../test';
 
-describe('index', () => {
+describe('global', () => {
   expect.extend({ toReplyWith });
   const sb = test.getStubbornInstance();
   const httpClient = test.getHttpClient();
@@ -64,29 +64,5 @@ describe('index', () => {
         });
       });
     });
-  });
-
-  it('should return NOT_IMPLEMENTED if no routes are configured', async () => {
-    const mockFn = jest.fn();
-    sb.on(EVENTS.NOT_IMPLEMENTED, mockFn);
-
-    expect(await httpClient.request({ path: '/' })).toReplyWith({
-      status: STATUS_CODES.NOT_IMPLEMENTED,
-    });
-    expect(mockFn).toHaveBeenCalledTimes(1);
-    const [dbg]: [Debugger] = mockFn.mock.calls[0];
-    const req = dbg.getInfo();
-    expect(req.method).toBe('GET');
-    expect(req.path).toBe('/');
-    expect(req.headers).toEqual({
-      accept: 'application/json',
-      'accept-encoding': expect.any(String),
-      connection: expect.any(String),
-      host: expect.any(String),
-      'user-agent': expect.any(String),
-    });
-    expect(req.query).toEqual({});
-    expect(req.body).toBeUndefined();
-    expect(req.hash).toBe('');
   });
 });
