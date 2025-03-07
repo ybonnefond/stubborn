03b63e4887fb377fa289c33f30659cad7c14c073
Yannick Bonnefond <yannickb@voodoo.io>
2025-03-07 16:09:01 +0100

    docs: update documentation

--- docs/js/menu-wc.js
+++ docs/js/menu-wc.js
@@ -1,19 +1,17 @@
 'use strict';
 
-customElements.define(
-  'compodoc-menu',
-  class extends HTMLElement {
+customElements.define('compodoc-menu', class extends HTMLElement {
     constructor() {
-      super();
-      this.isNormalMode = this.getAttribute('mode') === 'normal';
+        super();
+        this.isNormalMode = this.getAttribute('mode') === 'normal';
     }
 
     connectedCallback() {
-      this.render(this.isNormalMode);
+        this.render(this.isNormalMode);
     }
 
     render(isNormalMode) {
-      let tp = lithtml.html(`
+        let tp = lithtml.html(`
         <nav>
             <ul class="list">
                 <li class="title">
@@ -21,11 +19,7 @@ customElements.define(
                 </li>
 
                 <li class="divider"></li>
-                ${
-                  isNormalMode
-                    ? `<div id="book-search-input" role="search"><input type="text" placeholder="Type to search"></div>`
-                    : ''
-                }
+                ${ isNormalMode ? `<div id="book-search-input" role="search"><input type="text" placeholder="Type to search"></div>` : '' }
                 <li class="chapter">
                     <a data-type="chapter-link" href="index.html"><span class="icon ion-ios-home"></span>Getting started</a>
                     <ul class="links">
@@ -54,81 +48,71 @@ customElements.define(
                                         <span class="icon ion-ios-list"></span>Dependencies
                                     </a>
                                 </li>
+                                <li class="link">
+                                    <a href="properties.html" data-type="chapter-link">
+                                        <span class="icon ion-ios-apps"></span>Properties
+                                    </a>
+                                </li>
                     </ul>
                 </li>
                     <li class="chapter">
-                        <div class="simple menu-toggler" data-toggle="collapse" ${
-                          isNormalMode
-                            ? 'data-target="#classes-links"'
-                            : 'data-target="#xs-classes-links"'
-                        }>
+                        <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ? 'data-target="#classes-links"' :
+                            'data-target="#xs-classes-links"' }>
                             <span class="icon ion-ios-paper"></span>
                             <span>Classes</span>
                             <span class="icon ion-ios-arrow-down"></span>
                         </div>
-                        <ul class="links collapse " ${
-                          isNormalMode
-                            ? 'id="classes-links"'
-                            : 'id="xs-classes-links"'
-                        }>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="classes-links"' : 'id="xs-classes-links"' }>
                             <li class="link">
-                                <a href="classes/Debugger.html" data-type="entity-link">Debugger</a>
+                                <a href="classes/Debugger.html" data-type="entity-link" >Debugger</a>
                             </li>
                             <li class="link">
-                                <a href="classes/Output.html" data-type="entity-link">Output</a>
+                                <a href="classes/ErrorRenderer.html" data-type="entity-link" >ErrorRenderer</a>
                             </li>
                             <li class="link">
-                                <a href="classes/Route.html" data-type="entity-link">Route</a>
+                                <a href="classes/InvalidRemoveAfterMatchingTimesParameterError.html" data-type="entity-link" >InvalidRemoveAfterMatchingTimesParameterError</a>
                             </li>
                             <li class="link">
-                                <a href="classes/Stubborn.html" data-type="entity-link">Stubborn</a>
+                                <a href="classes/Output.html" data-type="entity-link" >Output</a>
+                            </li>
+                            <li class="link">
+                                <a href="classes/Route.html" data-type="entity-link" >Route</a>
+                            </li>
+                            <li class="link">
+                                <a href="classes/Stubborn.html" data-type="entity-link" >Stubborn</a>
                             </li>
                         </ul>
                     </li>
                     <li class="chapter">
-                        <div class="simple menu-toggler" data-toggle="collapse" ${
-                          isNormalMode
-                            ? 'data-target="#interfaces-links"'
-                            : 'data-target="#xs-interfaces-links"'
-                        }>
+                        <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ? 'data-target="#interfaces-links"' :
+                            'data-target="#xs-interfaces-links"' }>
                             <span class="icon ion-md-information-circle-outline"></span>
                             <span>Interfaces</span>
                             <span class="icon ion-ios-arrow-down"></span>
                         </div>
-                        <ul class="links collapse " ${
-                          isNormalMode
-                            ? ' id="interfaces-links"'
-                            : 'id="xs-interfaces-links"'
-                        }>
+                        <ul class="links collapse " ${ isNormalMode ? ' id="interfaces-links"' : 'id="xs-interfaces-links"' }>
                             <li class="link">
-                                <a href="interfaces/BodyDefinitionArray.html" data-type="entity-link">BodyDefinitionArray</a>
+                                <a href="interfaces/BodyDefinitionArray.html" data-type="entity-link" >BodyDefinitionArray</a>
                             </li>
                             <li class="link">
-                                <a href="interfaces/DiffError.html" data-type="entity-link">DiffError</a>
+                                <a href="interfaces/DiffError.html" data-type="entity-link" >DiffError</a>
                             </li>
                             <li class="link">
-                                <a href="interfaces/Emitter.html" data-type="entity-link">Emitter</a>
+                                <a href="interfaces/Emitter.html" data-type="entity-link" >Emitter</a>
                             </li>
                             <li class="link">
-                                <a href="interfaces/JsonArray.html" data-type="entity-link">JsonArray</a>
+                                <a href="interfaces/JsonArray.html" data-type="entity-link" >JsonArray</a>
                             </li>
                         </ul>
                     </li>
                     <li class="chapter">
-                        <div class="simple menu-toggler" data-toggle="collapse" ${
-                          isNormalMode
-                            ? 'data-target="#miscellaneous-links"'
-                            : 'data-target="#xs-miscellaneous-links"'
-                        }>
+                        <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ? 'data-target="#miscellaneous-links"'
+                            : 'data-target="#xs-miscellaneous-links"' }>
                             <span class="icon ion-ios-cube"></span>
                             <span>Miscellaneous</span>
                             <span class="icon ion-ios-arrow-down"></span>
                         </div>
-                        <ul class="links collapse " ${
-                          isNormalMode
-                            ? 'id="miscellaneous-links"'
-                            : 'id="xs-miscellaneous-links"'
-                        }>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="miscellaneous-links"' : 'id="xs-miscellaneous-links"' }>
                             <li class="link">
                                 <a href="miscellaneous/enumerations.html" data-type="entity-link">Enums</a>
                             </li>
@@ -149,7 +133,6 @@ customElements.define(
             </ul>
         </nav>
         `);
-      this.innerHTML = tp.strings;
+        this.innerHTML = tp.strings;
     }
-  },
-);
+});
